f6ee1b0a0abacefe32b77437a9c139cbc54d1a16
WaltEhren <waltehren@gmail.com>
2019-04-01 12:07:46 +0200

    Aufgabe c geloest, added what gelernt to Uebungen

---
+++ Uebungen/Uebung1/Cschalter.c
@@ -0,0 +1,30 @@
+#define F_CPU 16000000L   
+
+#include <avr/io.h>         
+#include <avr/cpufunc.h>   
+#include <util/delay.h>     
+
+void main(void) 
+{
+    
+    int pressed = 0;
+
+    DDRD = (1<<PD5);
+
+    while(1) 
+    {  
+        while(PIND & (1<<PD2)) 
+        {
+            PORTD = (1<<PD5);
+            _delay_ms(10);
+        } 
+        PORTD = 0;
+        _delay_ms(10);
+    }
+}
+
+// Heute haben wir gelernt:
+
+// 1. Immer nach Portaenderung ein delay einfuegen.
+// 2. In der Regel den Tastenzustand in while definieren
+//    Kein If else fuer zustandsabfragen 
